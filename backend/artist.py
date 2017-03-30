a416864b0c26313dfd4cee06cbc89c6dbba60f02
jcwall <johncodywall@gmail.com>
2017-03-29 18:18:34 -0600

    cleaned up class and code to remove spotipy wrapper

--- backend/artist.py
+++ backend/artist.py
@@ -1,16 +1,18 @@
 import spotipy
+import requests
 
 class Artist:
-    def __init__(self, artisturi):
-        sp = spotipy.Spotify()
-        artist = sp.artist(artisturi)
+    def __init__(self, artistid):
+        url = 'https://api.spotify.com/v1/artists/{}'.format(artistid)
+        artist = requests.get(url).json()
         self.id = artist['id'].encode('utf-8')
         self.uri = artist['uri'].encode('utf-8')
         self.name = artist['name'].encode('utf-8')
         self.genres = [genre.encode('utf-8') for genre in artist['genres']]
         self.followers = artist['followers']['total']#.astype(str)
-        simartists = sp.artist_related_artists(artist['id'])
-        self.related = [artist['name'].encode('utf-8') for artist in simartists['artists']]
-        self.relatedid = [artist['uri'].encode('utf-8') for artist in simartists['artists']]
+        url2 = url = 'https://api.spotify.com/v1/artists/{}/related-artists'.format(self.id)
+        simartists = requests.get(url2).json()['artists']
+        self.related = [artist['name'].encode('utf-8') for artist in simartists]
+        self.relatedid = [artist['id'].encode('utf-8') for artist in simartists]
+        self.relateduri = [artist['uri'].encode('utf-8') for artist in simartists]
         self.popularity = artist['popularity']
-        self.type = artist['type']
--- backend/spotify_scraper.py
+++ backend/spotify_scraper.py
@@ -1,6 +1,3 @@
-import spotipy
-import numpy as np
-import pandas as pd
 import pymongo
 from pymongo import MongoClient
 client = MongoClient()
@@ -8,8 +5,6 @@ db = client['spotifydb']
 tab = db['artist_list']
 from artist import Artist
 import requests
-import pickle
-
 
 def createdb(baseart):
     first = {'name': baseart.name, 'uri' : baseart.uri, 'followers': baseart.followers, 'genres': baseart.genres, 'popularity': baseart.popularity}
@@ -19,66 +14,31 @@ def createdb(baseart):
         next_artist = {'name': a.name, 'uri' : a.uri, 'followers': a.followers, 'genres': a.genres, 'type': a.type, 'popularity': a.popularity}
         tab.insert_one(next_artist)
 
-def uris_base():
-    return [doc['uri'].encode('utf-8') for doc in tab.find()]
+def get_mainstream_ids(artist):
+    url = 'https://api.spotify.com/v1/search?q={}&type=artist'.format(artist)
+    res = requests.get(url).json()['artists']['items']
+    searchresults = [i['id'] for i in res]
+    return searchresults[0]
 
-def collect_uris(init_uris):
-    uris_dump = set(init_uris)
-    update_list = []
-    for i in uris_dump:
-        a = Artist(i)
-        update_list.extend([ _ for _ in a.relatedid])
-    uris_dump.update(update_list)
-    print 'uri current length:', len(uris_dump)
-    return uris_dump
-
-def get_mainstream_uris(artistname):
-    url = 'https://api.spotify.com/v1/search?q={}&type=artist'.format(artistname)
-    res = requests.get(url)
-    #return res['artists']['items'][0]['uri'].encode('utf-8')
-    return res.json()['artists']['items'][0]['uri'].encode('utf-8')
-
-def mainstream_uris_db(mainstream_uri):
-    a = Artist(mainstream_uri)
-    init_uris = a.relatedid
-    uri_db = collect_uris(init_uris)
-    uri_db_final = collect_uris(uri_db)
-    return uri_db_final
-
-# def genres_split(df):
-#     genres = set()
-#     for m in df.genres:
-#         genres.update(g for g in m.split(','))
-#         genres = sorted(genres)
-    # for genre in genres:
-    #     data[genre] = [genre in movie.split('|') for movie in data.genres]
+def mainstream_simartists(mainstream_id):
+    a = Artist(mainstream_id)
+    sim_ids = a.relatedid
+    ids = set(sim_ids)
+    for i in sim_ids:
+        sim = Artist(i)
+        ids.update([_ for _ in sim.relatedid])
+    final_ids = set()
+    for i in ids:
+        sim = Artist(i)
+        final_ids.update([_ for _ in sim.relateduri])
+    return final_ids
 
 
 if __name__ == '__main__':
-    #RHCP database addition
-    init_rhcp_uri = 'spotify:artist:0L8ExT028jH3ddEcZwqJJ5'
-    baseart = Artist(init_rhcp_uri)
-    # init_uris = uris_base()
-    # uris_rhcp = collect_uris(init_uris)
-    # uris_iter = collect_uris(uris_rhcp)
-    # uris_rhcp.update(uris_iter)
-    #getting mainstream cluster uris
-    # mainstream = ['daft+punk', 'beyonce', 'the+beatles', 'kendrick+lamar', 'radiohead', 'luke+bryan']
-    # mainstream2 = ['skrillex', 'led+zeppelin', 'adele', 'mumford+&+sons', '2pac', 'elton+john']
-    # mainstream3 = ['taking+back+sunday', 'nelly', 'the+chainsmokers', 'ed+sheeran', 'carrie+underwood', 'lady+gaga', 'death+cab+for+cutie', 'bob+marley', 'outkast', 'spice+girls', 'amy+winehouse', 'michael+jackson']
-    # mainstream4 = ['red+hot+chili+peppers', 'pitbull', 'eminem', 'a+tribe+called+quest', 'the+xx', 'miles+davis', 'jimi+hendrix', 'alabama+shakes', 'usher', 'tim+mcgraw', 'ace+of+base']
-    # mainstream5 = ['deadmau5', 'kings+of+leon', 'drake', 'taylor+swift', 'arctic+monkeys', 'r.+kelly', 'dierks+bentley', 'britney+spears', 'fall+out+boy', 'enrique+iglesias', 'paramore', 'hozier']
-    # midstream = ['spoon', 'lcd+soundsystem', 'sufjan+stevens', 'the+white+stripes', 'arcade+fire', 'erykah+badu', 'bon+iver', 'the+eagles', 'linkin+park', 'norah+jones', 'josh+groban', 'odesza']
-    # midstream2 = ['tame+impala', 'chris+stapleton', 'my+morning+jacket', 'sturgill+simpson', 'ludacris', 'cake', 'beastie+boys', 'no+doubt', 'nine+inch+nails', 'disclosure', 'leon+bridges']
-    # midstream3 = ['father+john+misty', 'porter+robinson', 'lorde', 'dj+khaled', 'bastille', 'gucci+mane', 'local+natives', 'mac+demarco', 'little+dragon', 'lettuce', 'easton corbin', 'a+tribe+called+quest', 'nine+inch+nails', 'beach+house', 'tool', 'phantogram', 'saint+motel', 'flume']
-    # mainstream6 = ['the+lumineers', 'the+black+eyed+peas', 'kanye+west', 'kelly+clarkson', 'justin+bieber']
-    mainstream7 = ['the+avett+brothers', 'metallica', 'bon+jovi', 'lil+wayne', 'mstrkrft', 'kehlani', 'tove+lo', 'bleachers', 'marshmello', 'cage+the+elephant', 'j+cole', 'major+lazer', 'm83', 'vic+mensa', 'daughter', 'cherub', 'ariana+grande', 'eric+church', 'chris+brown']
-    res_list = []
-    for i in mainstream7:
-        res_list.append(get_mainstream_uris(i))
-    #populate individual mainstream related dbs
-    set_of_uris = set()
-    for i in res_list:
-        set_of_uris.update(mainstream_uris_db(i))
-        print 'artist {} associated uris collected'.format(i)
-        print 'total uri length', len(set_of_uris)
+    init_rhcp_uri = '0L8ExT028jH3ddEcZwqJJ5'
+    #baseart = Artist(init_rhcp_uri)
+    #createdb(baseart)
+    # res_list = [get_mainstream_ids(artist) for artist in mainstream10]
+    # set_of_uris = set()
+    # for i in res_list:
+    #     set_of_uris.update(mainstream_simartists(i))
